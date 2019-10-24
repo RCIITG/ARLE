d39107bc98614e7aab867b7aac0bb575494a49b6
Tejasvee Panwar <panwar.tejasvee@gmail.com>
2019-10-24 13:04:59 +0530

    Added code for new motor driver

---
+++ RMCS2301_RMCS2004/new_driver/README.md
@@ -0,0 +1,2 @@
+## RMCS 2004 Motor
+Previously used motor driver (RMCS 2301) works erratically. Code for a different motor driver. 
---
+++ RMCS2301_RMCS2004/new_driver/motor_pwm_control.ino
@@ -0,0 +1,16 @@
+
+
+
+void setup() {
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  digitalWrite(8,LOW);
+  analogWrite(9,128);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+
+  
+}
